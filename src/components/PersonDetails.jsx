aacb43455c319d93fd73bef6e85a6a7464c833cb
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-04-11 21:15:46 +0300

    Some tailwind wtyle

--- src/components/PersonDetails.jsx
+++ src/components/PersonDetails.jsx
@@ -5,20 +5,30 @@ function PersonDetails({ person }) {
 
   return (
     <div>
-      <h2>PersonDetails</h2>
-      <img src={profile_image_url} />
-      <div>
-        <p>{name}</p>
-      </div>
-      <div>
-        <p>City</p>
-        <span>{city}</span>
-        <p>Age</p>
-        <span>{age}</span>
-        <p>Interests:</p>
-        <span>{interest}</span>
-        <p>{motto}</p>
-      </div>
+      <section className="flex flex-col items-center">
+        <h2>PersonDetails</h2>
+        <div style={{ width: "50vw" }}>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img src={profile_image_url} />
+          </div>
+
+          <p>{name}</p>
+
+          <div className="flex justify-between">
+            <p>City:</p>
+            <p>{city}</p>
+          </div>
+          <div className="flex">
+            <p>Age</p>
+            <span>{age}</span>
+          </div>
+          <div style={{ width: "50%" }} className="flex gap-60">
+            <p>Interests:</p>
+            <span>{interest}</span>
+          </div>
+          <p>{motto}</p>
+        </div>
+      </section>
     </div>
   );
 }
