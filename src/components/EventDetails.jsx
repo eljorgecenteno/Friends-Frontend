72e98ed7eb2db7f9da8a958e40f9f8a3d6240405
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-04-20 20:34:17 +0300

    eventdetails

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -5,31 +5,35 @@ function EventDetails({ meetup }) {
   const { name, profile_image_url, interest, description, date, opinions, persons, city } = meetup;
 
   return (
-    <div>
-      <h1 id="header">
-        <strong>{name}</strong>
-      </h1>
-      <div>
-        <img src={profile_image_url} className="person-picture" />
-      </div>
-      <div id="city">
-        <p>City:</p>
-        <p>{city}</p>
-      </div>
-      <div id="city">
-        <p>Date:</p>
-        <p>{`${date.$d}-${date.$m}-${date.$y}`}</p>
-      </div>
-      <div id="interests">
-        <p>Interest:</p>
-        <p>{interest}</p>
-      </div>
-      <div id="opinions">
-        <p>Opinion:</p>
-        <p>{opinions.map((oneOpinion)=> {
-            return oneOpinion.description
-        })}</p>
-      </div>
+    <div id="container">
+      <section id="event-details-container">
+        <h1 id="header">
+          <strong>{name}</strong>
+        </h1>
+        <div>
+          <img src={profile_image_url} className="person-picture" />
+        </div>
+        <div id="city">
+          <p>City:</p>
+          <p>{city}</p>
+        </div>
+        <div id="city">
+          <p>Date:</p>
+          <p>{`${date.$d}-${date.$m}-${date.$y}`}</p>
+        </div>
+        <div id="interests">
+          <p>Interest:</p>
+          <p>{interest}</p>
+        </div>
+        <div id="interests">
+          <p>Opinion:</p>
+          <p>
+            {opinions.map((oneOpinion) => {
+              return oneOpinion.description;
+            })}
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
