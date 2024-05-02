ded9e425e00951aad446516717505db794ed225b
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-05-02 20:07:07 +0300

    edit

--- src/Pages/DicoverEventsPage.jsx
+++ src/Pages/DicoverEventsPage.jsx
@@ -1,13 +1,12 @@
-
 import axios from "axios";
 import "./DiscoverEventsPage.css";
 import { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
 function DiscoverEventsPage() {
   const [allEvents, setAllEvents] = useState([]);
@@ -18,7 +17,7 @@ function DiscoverEventsPage() {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [Chess, setChess] = useState(false);
-  
+
   const ChessHandleChange = (e) => {
     setChess(e.target.checked);
   };
@@ -129,285 +128,195 @@ function DiscoverEventsPage() {
     if (Literature === true) {
       selectedInterests.push("Literature");
     }
-    
-    setInterest(selectedInterests);
 
-    
+    setInterest(selectedInterests);
   };
 
   useEffect(() => {
     let filteredPersons = allEvents.filter((oneEvent) => {
       let interestMet = false;
-      let earliestDateAproval = false
-      let latestDateAproval = false
-      let cityAproval = false
+      let earliestDateAproval = false;
+      let latestDateAproval = false;
+      let cityAproval = false;
 
-if( city === ""){
-  cityAproval = true
-} else if (oneEvent.city === city){
-  cityAproval = true
-}
-    
+      if (city === "") {
+        cityAproval = true;
+      } else if (oneEvent.city === city) {
+        cityAproval = true;
+      }
+
       if (interest.length === 0) {
         interestMet = true;
-        console.log(interest)
+        console.log(interest);
       }
       if (interest.length > 0) {
-        console.log(interest)
+        console.log(interest);
         for (let i = 0; i < interest.length; i++) {
           if (oneEvent.interest.includes(interest[i])) {
             interestMet = true;
           }
         }
       }
-   
-    
-     if(startDate === null){
-      earliestDateAproval = true
-     } else if (oneEvent.date.$y > startDate.$y){
-        earliestDateAproval = true  
-      } else if (oneEvent.date.$y === startDate.$y){
+
+      if (startDate === null) {
+        earliestDateAproval = true;
+      } else if (oneEvent.date.$y > startDate.$y) {
+        earliestDateAproval = true;
+      } else if (oneEvent.date.$y === startDate.$y) {
         if (oneEvent.date.$m > startDate.$M) {
-          earliestDateAproval = true 
-        } else if (oneEvent.date.$m === startDate.$M){
-          if (oneEvent.date.$d > startDate.$D){
-            earliestDateAproval = true 
-          } else if (oneEvent.date.$d === startDate.$D){
-            earliestDateAproval = true 
+          earliestDateAproval = true;
+        } else if (oneEvent.date.$m === startDate.$M) {
+          if (oneEvent.date.$d > startDate.$D) {
+            earliestDateAproval = true;
+          } else if (oneEvent.date.$d === startDate.$D) {
+            earliestDateAproval = true;
           }
         }
       }
-console.log(oneEvent)
-console.log(endDate)
-if(endDate === null){
-  latestDateAproval = true
- } else if (oneEvent.date.$y < endDate.$y){
-        latestDateAproval = true  
-      } else if (oneEvent.date.$y === endDate.$y){
+      console.log(oneEvent);
+      console.log(endDate);
+      if (endDate === null) {
+        latestDateAproval = true;
+      } else if (oneEvent.date.$y < endDate.$y) {
+        latestDateAproval = true;
+      } else if (oneEvent.date.$y === endDate.$y) {
         if (oneEvent.date.$m < endDate.$M) {
-          latestDateAproval = true 
-        } else if (oneEvent.date.$m === endDate.$M){
-          if (oneEvent.date.$d < endDate.$D){
-            latestDateAproval = true 
-          } else if (oneEvent.date.$d === endDate.$D){
-            latestDateAproval = true 
+          latestDateAproval = true;
+        } else if (oneEvent.date.$m === endDate.$M) {
+          if (oneEvent.date.$d < endDate.$D) {
+            latestDateAproval = true;
+          } else if (oneEvent.date.$d === endDate.$D) {
+            latestDateAproval = true;
           }
         }
       }
 
-      return (
-         cityAproval&&
-        interestMet && earliestDateAproval && latestDateAproval
-      );
+      return cityAproval && interestMet && earliestDateAproval && latestDateAproval;
+    });
+    setseeInterest(false);
 
-        });
-        setseeInterest(false)
-    
     setAllFilteredpersons(filteredPersons);
-  }, [interest,]);
+  }, [interest]);
 
-
-
-  function seeOptions (){
- if (seeInterest === false){
-  setseeInterest(true)
-  
- } else {
-  setseeInterest(false)
-  
- }
+  function seeOptions() {
+    if (seeInterest === false) {
+      setseeInterest(true);
+    } else {
+      setseeInterest(false);
+    }
   }
- 
+
   return (
     <>
-
       <div id="discover-persons-container">
-      <h2 style={{ marginRight: "40px" }}>Filter by</h2>
-          <h3>Theme:</h3>
-          <button id="see-interest-discover-page"onClick={seeOptions}>Choose themes ▼</button>
+        <h2 style={{ marginRight: "40px" }}>Filter by</h2>
+        <h3>Theme:</h3>
+        <button id="see-interest-discover-page" onClick={seeOptions}>
+          Choose themes ▼
+        </button>
         <form onSubmit={handleSubmit}>
-        <div id="discover-persons-form-container">
-          
-          {seeInterest && 
-          <div >
-                  <input
-              type="checkbox"
-              name="Chess"
-              checked={Chess}
-              onChange={ChessHandleChange}
-            ></input>
-            <label htmlFor="Chess">Chess</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Cinema"
-              checked={Cinema}
-              onChange={cinemaHandleChange}
-            ></input>
-            <label htmlFor="Cinema"> Cinema</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Poker"
-              checked={Poker}
-              onChange={pokerHandleChange}
-            ></input>
-            <label htmlFor="Poker"> Poker</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Theater"
-              checked={Theater}
-              onChange={theaterHandleChange}
-            ></input>
-            <label htmlFor="Theater"> Theater</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Party"
-              checked={Party}
-              onChange={partyHandleChange}
-            ></input>
-            <label htmlFor="Party"> Party</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Restaurants"
-              checked={Restaurants}
-              onChange={restaurantshandleChange}
-            ></input>
-            <label htmlFor="Restaurants"> Restaurants</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Hiking"
-              checked={Hiking}
-              onChange={hikingHandleChange}
-            ></input>
-            <label htmlFor="Hiking"> Hiking</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Football"
-              checked={Football}
-              onChange={footballHandleChange}
-            ></input>
-            <label htmlFor="Football"> Football</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="MovieNight"
-              checked={MovieNight}
-              onChange={movieNightHandleChange}
-            ></input>
-            <label htmlFor="Movie Night"> Movie Night</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Running"
-              checked={Running}
-              onChange={runningHandleChange}
-            ></input>
-            <label htmlFor="Running"> Running</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Language exchange"
-              checked={LanguageExchange}
-              onChange={languageExchangeHandleChange}
-            ></input>
-            <label htmlFor="Language exchange"> Language exchange</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Trips"
-              checked={Trips}
-              onChange={tripsHandleChange}
-            ></input>
-            <label htmlFor="Trips"> Trips</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Basketball"
-              checked={Basketball}
-              onChange={basketballHandleChange}
-            ></input>
-            <label htmlFor="Basketball"> Basketball</label>
-            <br></br>
-            <input
-              type="checkbox"
-              name="Literature"
-              checked={Literature}
-              onChange={literatureHandleChange}
-            ></input>
-            <label htmlFor="Literature"> Literature</label>
-            <br></br>
-            </div>}
-            
-              <h3 style={{ marginLeft: "110px" }}>City:</h3>
-              <select
-                name="cityselected"
-                onChange={(e) => setCity(e.target.value)}
-                id="discover-persons-select-city"
-                defaultValue=""
-                
-              >
-                <option value="" disabled hidden>Choose a city</option>
-                <option value="London">London</option>
-                <option value="Paris">Paris</option>
-                <option value="Madrid">Madrid</option>
-                <option value="Berlin">Berlin</option>
-                <option value="Athens">Athens</option>
-              </select>
-              <h3 style={{ marginRight: "20px" }}></h3>
-              <div  id="calendar-container-events-page">
-                <h3 style={{ marginRight: "15px" }}>Dates:</h3>
+          <div id="discover-persons-form-container">
+            {seeInterest && (
+              <div>
+                <input type="checkbox" name="Chess" checked={Chess} onChange={ChessHandleChange}></input>
+                <label htmlFor="Chess">Chess</label>
+                <br></br>
+                <input type="checkbox" name="Cinema" checked={Cinema} onChange={cinemaHandleChange}></input>
+                <label htmlFor="Cinema"> Cinema</label>
+                <br></br>
+                <input type="checkbox" name="Poker" checked={Poker} onChange={pokerHandleChange}></input>
+                <label htmlFor="Poker"> Poker</label>
+                <br></br>
+                <input type="checkbox" name="Theater" checked={Theater} onChange={theaterHandleChange}></input>
+                <label htmlFor="Theater"> Theater</label>
+                <br></br>
+                <input type="checkbox" name="Party" checked={Party} onChange={partyHandleChange}></input>
+                <label htmlFor="Party"> Party</label>
+                <br></br>
+                <input type="checkbox" name="Restaurants" checked={Restaurants} onChange={restaurantshandleChange}></input>
+                <label htmlFor="Restaurants"> Restaurants</label>
+                <br></br>
+                <input type="checkbox" name="Hiking" checked={Hiking} onChange={hikingHandleChange}></input>
+                <label htmlFor="Hiking"> Hiking</label>
+                <br></br>
+                <input type="checkbox" name="Football" checked={Football} onChange={footballHandleChange}></input>
+                <label htmlFor="Football"> Football</label>
+                <br></br>
+                <input type="checkbox" name="MovieNight" checked={MovieNight} onChange={movieNightHandleChange}></input>
+                <label htmlFor="Movie Night"> Movie Night</label>
+                <br></br>
+                <input type="checkbox" name="Running" checked={Running} onChange={runningHandleChange}></input>
+                <label htmlFor="Running"> Running</label>
+                <br></br>
+                <input type="checkbox" name="Language exchange" checked={LanguageExchange} onChange={languageExchangeHandleChange}></input>
+                <label htmlFor="Language exchange"> Language exchange</label>
+                <br></br>
+                <input type="checkbox" name="Trips" checked={Trips} onChange={tripsHandleChange}></input>
+                <label htmlFor="Trips"> Trips</label>
+                <br></br>
+                <input type="checkbox" name="Basketball" checked={Basketball} onChange={basketballHandleChange}></input>
+                <label htmlFor="Basketball"> Basketball</label>
+                <br></br>
+                <input type="checkbox" name="Literature" checked={Literature} onChange={literatureHandleChange}></input>
+                <label htmlFor="Literature"> Literature</label>
+                <br></br>
+              </div>
+            )}
+
+            <h3 style={{ marginLeft: "110px" }}>City:</h3>
+            <select name="cityselected" onChange={(e) => setCity(e.target.value)} id="discover-persons-select-city" defaultValue="">
+              <option value="" disabled hidden>
+                Choose a city
+              </option>
+              <option value="London">London</option>
+              <option value="Paris">Paris</option>
+              <option value="Madrid">Madrid</option>
+              <option value="Berlin">Berlin</option>
+              <option value="Athens">Athens</option>
+            </select>
+            <h3 style={{ marginRight: "20px" }}></h3>
+            <div id="calendar-container-events-page">
+              <h3 style={{ marginRight: "15px" }}>Dates:</h3>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer  components={['DateTimePicker']}>
-        <DateTimePicker   onChange={(value) => setStartDate(value)} label="Earlier date"  views={['year', 'month', 'day']}/>
-      </DemoContainer>
-    </LocalizationProvider>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer  components={['DateTimePicker']}>
-        <DateTimePicker    onChange={(value) => setEndDate(value)} label="Latest date"  views={['year', 'month', 'day']}/>
-      </DemoContainer>
-    </LocalizationProvider>
-    </div>
-          
-          <button type="submit" id="discover-persons-filter-button">Search</button>
-        </div>
-        </form>
-        
-         
-          </div>
-     <div id="eachPersonContainer">
-      {allEvents &&
-     
-        allFilteredPersons.map((eachEvent) => {
-          
-          return (
-            <Link key={eachEvent._id} to={`/events/${eachEvent._id}`}  id="eachPerson" >
-            <div   >
-              <img
-                src={eachEvent.profile_image_url}
-                alt={`Profile image of ${eachEvent.name}`}
-              />
-              
-              <h3 id="eachPerson-age-and-name-discover-persons-page">{eachEvent.name}</h3>
-            
-              <h3> {eachEvent.city}</h3>
-              <p>{`${eachEvent.date.$d}/${eachEvent.date.$m + 1}/${eachEvent.date.$y}`}</p>
-              <h4> {`Theme: ${eachEvent.interest}`}</h4>
-               
-              
-              <p>{eachEvent.description}</p>
-              
+                <DemoContainer components={["DateTimePicker"]}>
+                  <DateTimePicker onChange={(value) => setStartDate(value)} label="Earlier date" views={["year", "month", "day"]} />
+                </DemoContainer>
+              </LocalizationProvider>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={["DateTimePicker"]}>
+                  <DateTimePicker onChange={(value) => setEndDate(value)} label="Latest date" views={["year", "month", "day"]} />
+                </DemoContainer>
+              </LocalizationProvider>
             </div>
-            </Link>
-          );
-         
-        })}</div>
-         
+
+            <button type="submit" id="discover-persons-filter-button">
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+      <div id="eachPersonContainer">
+        {allEvents &&
+          allFilteredPersons.map((eachEvent) => {
+            console.log(eachEvent);
+
+            return (
+              <Link key={eachEvent._id} to={`/events/${eachEvent._id}`} id="eachPerson">
+                <div>
+                  <img src={eachEvent.profile_image_url} alt={`Profile image of ${eachEvent.name}`} />
+
+                  <h3 id="eachPerson-age-and-name-discover-persons-page">{eachEvent.name}</h3>
+
+                  <h3> {eachEvent.city}</h3>
+                  <p>{`${eachEvent.date?.$d}/${eachEvent.date?.$m + 1}/${eachEvent.date?.$y}`}</p>
+                  <h4> {`Theme: ${eachEvent.interest}`}</h4>
+
+                  <p>{eachEvent.description}</p>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
     </>
   );
 }
